5a3b1510ffcaae72b2178b817b39bdceea28e9c4
Vebjørn Ohr <vebjorn.ohr@gmail.com>
2020-10-05 15:03:54 +0200

    Add basic bot

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+import random
+
+from dotenv import load_dotenv
+from discord.ext import commands
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+bot = commands.Bot(command_prefix='-')
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server'
+    )
+
+
+@bot.command(name='99')
+async def nine_nine(ctx):
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+
+@bot.command(name='roll', help='Simulates rolling dice.')
+async def roll(ctx, number_of_sides: int = 6,  number_of_dice: int = 1):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
+bot.run(TOKEN)
